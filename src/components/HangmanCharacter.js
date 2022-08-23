d9356f1bcbf16de4f5e706bf585a5daf37ddf1cb
Osama Ehsan <osamaehsan66@gmail.com>
2022-08-23 21:05:27 +0500

    Character update on wrong guess

--- src/components/HangmanCharacter.js
+++ src/components/HangmanCharacter.js
@@ -1,22 +1,24 @@
 import React from 'react'
 
-function HangmanCharacter() {
+function HangmanCharacter(props) {
+  const {wrongGuesses} = props;
+  
   return (
     <div className='hangman-character'>
       <div className='hgmc__top'> </div>
       <div className='hgmc__stand'> </div>
       <div className='hgmc__base'> </div> 
       <div className='hgmc__rope'> </div>
-
-      <div className='hgmc__head'> </div>
-      <div className='hgmc__body'> </div>
+      {wrongGuesses.length >= 1 && <div className='hgmc__head'> </div> }
+      
+      {wrongGuesses.length >= 2 && <div className='hgmc__body'> </div>}
       <div className='hgmc__legs'>
-        <div className='hgmc__leg hgmc__leg--left'></div>
-        <div className='hgmc__leg hgmc__leg--right'></div>
+        {wrongGuesses.length >= 3 && <div className='hgmc__leg hgmc__leg--left'></div>}
+        {wrongGuesses.length >= 4 && <div className='hgmc__leg hgmc__leg--right'></div>}
       </div>
       <div className='hgmc__arms'>
-        <div className='hgmc__arm hgmc__arm--left'></div>
-        <div className='hgmc__arm hgmc__arm--right'></div>
+        {wrongGuesses.length >= 5 && <div className='hgmc__arm hgmc__arm--left'></div>}
+        {wrongGuesses.length >= 6 && <div className='hgmc__arm hgmc__arm--right'></div>}
       </div>
     </div>
   )
